dc4e3870830b8dc862abde4fde43becc4b53914e
Vladimir Kadalashvili <Kadalashvili.Vladimir@gmail.com>
2019-04-30 22:41:11 +0500

    Changed directory structure

---
+++ copyscape_api/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='copyscape-api-client',  
+    version='0.1',
+    scripts=[],
+    author="Vladimir Kadalashvili",
+    author_email="Kadalashvili.Vladimir@gmail.com",
+    description="Python client for Copyscape API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/selentium/copyscape-api-client",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests",
+        "xmltodict",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+ )
